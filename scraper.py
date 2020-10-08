c04a2f602310d4caaddbd0f5fbe2bf141033e5e7
Tarlex <Tarlex.py@yandex.ru>
2020-10-08 19:31:03 +0300

    add sort

--- scraper.py
+++ scraper.py
@@ -2,14 +2,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 from collections import Counter
+import operator
 
-html = urlopen('https://docs.python.org/3/library/multiprocessing.html')
-soup = BeautifulSoup(html, 'html.parser')
 
-def getText(html):
+def getText(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    fout = open('scraper', 'w+')
     
-    fout = open('scraper', 'wt')
-
     for tag in soup.find_all(re.compile("^p")):
         text = tag.text
         print(text, file=fout)
@@ -33,17 +33,20 @@ def search():
                 clear_word += letter.lower()
         word_list.append(clear_word)
 
-    def display_inventory(inventory):
-        for k, v in inventory.items():
+    def display_inventory(inventory): 
+        sort_items = sorted(inventory.items(), key = operator.itemgetter(1)) # .itemgetter(1) - sorted on values, with 0 - keys
+
+        for k, v in sort_items:
             if v > 20 and len(k) > 3 :
-                print(str(v) + ' ' + k)
+                print(str(v) + '   ' + k)
             else: pass
-
-
 
     lis = Counter(word_list)
 
-    print(lis, file=resaultfile)
+    print(lis, file=resaultfile) 
     display_inventory(lis)
 
-search()
+if __name__ == "__main__": 
+    url = 'https://docs.python.org/3/library/multiprocessing.html'
+    getText(url)
+    search()
